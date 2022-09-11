e3894fc953f0283312481b9dad4cec3c9e50f2cd
Brooks <brooks@karlik.org>
2022-09-11 16:28:22 -0700

    initial impl

---
+++ src/error.rs
@@ -0,0 +1,181 @@
+use std::io;
+use derive_more::{Constructor, From};
+use quick_xml::events::BytesStart;
+
+use std::string::FromUtf8Error;
+use std::path::PathBuf;
+
+type StaticEvent = quick_xml::events::Event<'static>;
+
+#[derive(thiserror::Error, Debug, From)]
+pub enum Error {
+    #[error("{0}")]
+    LeadingEvents(LeadingEvents),
+    #[error("{0}")]
+    Layer(LayerError),
+    #[error("{0}")]
+    TrailingEvents(TrailingEvents),
+    #[error("{0}")]
+    ParseLayer(ParseLayer),
+}
+
+#[derive(thiserror::Error, Debug)]
+#[error("Failed to write leading event `{event:?}` - error: `{err}`")]
+pub struct LeadingEvents {
+    pub(crate) err: quick_xml::Error,
+    pub(crate) event: StaticEvent
+}
+
+#[derive(thiserror::Error, Debug, From)]
+#[error("Error writing layer: {0}")]
+pub enum LayerError {
+    Header(LayerHeader),
+    Body(LayerBody),
+    Footer(LayerFooter),
+}
+
+#[derive(thiserror::Error, Debug)]
+#[error("Failed to write header for layer: `{header:?}` - error: `{err}`")]
+pub struct LayerHeader {
+    pub(crate) err: quick_xml::Error,
+    pub(crate) header: StaticEvent
+}
+
+#[derive(thiserror::Error, Debug)]
+#[error("Failed to write object in layer body: {object:?} - error: `{err}`")]
+pub struct LayerBody {
+    pub(crate) err: quick_xml::Error,
+    pub(crate) object: StaticEvent
+}
+
+#[derive(thiserror::Error, Debug, Constructor)]
+#[error("Failed to write footer for layer: {footer:?} - error: `{err}`")]
+pub struct LayerFooter {
+    pub(crate) err: quick_xml::Error,
+    pub(crate) footer: StaticEvent
+}
+
+#[derive(thiserror::Error, Debug, Constructor)]
+#[error("Failed to write trailing event `{event:?}` - error: `{err}`")]
+pub struct TrailingEvents {
+    pub(crate) err: quick_xml::Error,
+    pub(crate) event: StaticEvent
+}
+
+#[derive(thiserror::Error, Debug, Constructor)]
+#[error("Id `{id}`was not found in document")]
+pub struct MissingId {
+    pub(crate) id: String,
+}
+
+#[derive(thiserror::Error, Debug, From)]
+pub enum ParseLayer {
+    #[error("failed to parse layer: `{0}`")]
+    MissingLayerEnd(MissingLayerEnd),
+    #[error("failed to parse layer: `{0}`")]
+    ParseObject(ParseObject),
+    #[error("failed to parse layer: `{0}`")]
+    MissingLayerName(MissingLayerName),
+}
+
+#[derive(thiserror::Error, Debug, Constructor)]
+#[error("Failed to parse object in layer {layer_name}: {error}")]
+pub struct ParseObject {
+    pub(crate) error: IdentifierError,
+    pub(crate) layer_name: String,
+}
+
+#[derive(thiserror::Error, Debug, Constructor)]
+#[error("Missing end of group attribute for layer {layer_name}")]
+pub struct MissingLayerEnd {
+    pub(crate) layer_name: String,
+}
+
+#[derive(thiserror::Error, Debug, Constructor)]
+#[error("Layer name was missing or not UTF8 for event: {event:?}")]
+pub struct MissingLayerName {
+    event: BytesStart<'static>,
+}
+
+#[derive(thiserror::Error, Debug, Constructor)]
+#[error("Failed to parse `{dimension}` parameter to utf8 string; error: `{error}`")]
+pub struct DimensionUtf8 {
+    error: FromUtf8Error,
+    dimension: DimensionOrId
+}
+
+#[derive(thiserror::Error, Debug, Constructor)]
+#[error("Failed to parse `{dimension}` parameter to utf8 string; error: `{error}`")]
+pub struct DimensionParse {
+    error: std::num::ParseFloatError,
+    dimension: DimensionOrId
+}
+
+#[derive(Debug, derive_more::Display)]
+pub enum DimensionOrId {
+    #[display(fmt = "width")]
+    Width,
+    #[display(fmt = "height")]
+    Height,
+    #[display(fmt = "id")]
+    Id
+}
+
+#[derive(thiserror::Error, Debug, From)]
+pub enum IdentifierError {
+    #[error("Failed to parse identifier: `{0}`")]
+    DimensionUtf8(DimensionUtf8),
+    #[error("Failed to parse identifier: `{0}`")]
+    DimensionParse(DimensionParse),
+    #[error("Failed to parse identifier: `{0}`")]
+    MissingObjectIdentifier(MissingObjectIdentifier),
+}
+
+
+#[derive(thiserror::Error, Debug, Constructor)]
+#[error("One of width ({width:?}) / height ({height:?}) / id ({id:?} was missing for element {element:?}")]
+pub struct MissingObjectIdentifier {
+    element: BytesStart<'static>,
+    width: Option<f64>,
+    height: Option<f64>,
+    id: Option<String>,
+}
+
+#[derive(thiserror::Error, Debug, From)]
+pub enum EncodingError {
+    #[error("Error while encoding image: `{0}`")]
+    OpenFile(OpenFile),
+    #[error("Error while encoding image: `{0}`")]
+    ReadBytes(ReadBytes),
+    #[error("Error while encoding image: `{0}`")]
+    UnknownMime(UnknownMime),
+    #[error("Error while encoding image: `{0}`")]
+    WrongEncoding(WrongEncoding),
+}
+
+
+#[derive(thiserror::Error, Debug, Constructor)]
+#[error("failed to open file at {}; error: {error}", "path.display()")]
+pub struct OpenFile {
+    error: io::Error,
+    path: PathBuf,
+}
+
+#[derive(thiserror::Error, Debug, Constructor)]
+#[error("failed to read bytes of file {} after it was opened; error: {error}", "path.display()")]
+pub struct ReadBytes {
+    error: io::Error,
+    path: PathBuf,
+}
+
+#[derive(thiserror::Error, Debug, Constructor)]
+#[error("image at path {} has an unknown mime type. figure_second only handles PNG encoded images", "path.display()")]
+pub struct UnknownMime {
+    path: PathBuf,
+}
+
+#[derive(thiserror::Error, Debug, Constructor)]
+#[error("image at path {} is not PNG encoded. Images must be png encoded currently", "path.display()")]
+pub struct WrongEncoding {
+    path: PathBuf,
+}
---
+++ src/lib.rs
@@ -0,0 +1,247 @@
+mod object;
+mod parse;
+mod error;
+
+use error::*;
+
+pub use object::EncodedImage;
+
+use quick_xml::events::Event;
+use std::io::BufRead;
+use std::io::Write;
+
+#[derive(Debug)]
+pub struct Inkscape {
+    leading_events: Vec<Event<'static>>,
+    layers: Vec<Layer>,
+    trailing_events: Vec<Event<'static>>,
+}
+
+#[derive(Debug)]
+pub struct Layer {
+    header: Event<'static>,
+    content: Vec<object::Object>,
+    footer: Event<'static>,
+}
+
+impl Layer {
+    #[cfg(test)]
+    fn eof_group_test(content: Vec<object::Object>) -> Self {
+        Self {
+            header: Event::Eof,
+            content,
+            footer: Event::Eof,
+        }
+    }
+}
+
+/// Export an [`Inkscape`] object to a file
+impl Inkscape {
+    pub fn write_svg<W: Write>(self, writer: W) -> Result<(), Error> {
+        let mut writer = quick_xml::Writer::new(writer);
+
+        for event in self.leading_events {
+            writer
+                .write_event(&event)
+                .map_err(|err| LeadingEvents { err, event })?;
+                //.with_context(|| format!("failed to write a leading event: {:?}", event))?;
+        }
+
+        for layer in self.layers {
+            writer.write_event(&layer.header)
+                .map_err(|err| LayerHeader { err, header: layer.header })
+                .map_err(LayerError::from)?;
+
+            for object in layer.content {
+                let event = object.into_event();
+                writer.write_event(&event)
+                    .map_err(|err| LayerBody {err, object: event})
+                    .map_err(LayerError::from)?;
+            }
+
+            writer.write_event(&layer.footer)
+                .map_err(|err| LayerFooter::new(err, layer.footer))
+                .map_err(LayerError::from)?;
+        }
+
+        for event in self.trailing_events {
+            writer
+                .write_event(&event)
+                .map_err(|err| TrailingEvents::new(err, event))?;
+        }
+
+        Ok(())
+    }
+
+    pub fn parse_svg<R: BufRead>(reader: R, buffer: &mut Vec<u8>) -> Result<Self, Error> {
+        let mut reader = quick_xml::Reader::from_reader(reader);
+
+        let (leading_events, first_group) = parse::leading_events(&mut reader, buffer);
+
+        // read the inner layers
+        let (layers, first_trailing) = if let Some(first_group) = first_group {
+            let (layers, first_trailing) = parse::layers(&mut reader, buffer, first_group)?;
+            (layers, Some(first_trailing))
+        } else {
+            (vec![], None)
+        };
+
+        let trailing_events = if let Some(first_trailing) = first_trailing {
+            parse::trailing_events(&mut reader, buffer, first_trailing)
+        } else {
+            Vec::new()
+        };
+
+        let inkscape = Inkscape {
+            leading_events,
+            layers,
+            trailing_events,
+        };
+        Ok(inkscape)
+    }
+
+    pub fn id_to_image(&mut self, id: &str, image: EncodedImage) -> Result<(), MissingId> {
+        for layer in &mut self.layers {
+            for object in layer.content.iter_mut() {
+                match object {
+                    object::Object::Rectangle(rect) => {
+                        if rect.ident.id == id {
+                            rect.set_image(image);
+
+                            return Ok(());
+                        }
+                    }
+                    object::Object::Image(img) => {
+                        if img.ident.id == id {
+                            img.update_image(image);
+
+                            return Ok(());
+                        }
+                    }
+                    object::Object::Other(_) => (),
+                };
+            }
+        }
+
+        Err(MissingId::new(id.into()))
+    }
+
+    pub fn dimensions(&mut self, id: &str) -> Result<(f64, f64), MissingId> {
+        for layer in &self.layers {
+            for object in &layer.content {
+                match object {
+                    object::Object::Rectangle(rect) => {
+                        if rect.ident.id == id {
+                            return Ok((rect.ident.width, rect.ident.height));
+                        }
+                    }
+                    object::Object::Image(img) => {
+                        if img.ident.id == id {
+                            return Ok((img.ident.width, img.ident.height));
+                        }
+                    }
+                    object::Object::Other(_) => (),
+                };
+            }
+        }
+
+        Err(MissingId::new(id.into()))
+    }
+
+    pub fn object_ids(&self) -> IdIterator<'_> {
+        IdIterator::new(&self.layers)
+    }
+
+    pub fn get_layers(&self) -> &[Layer] {
+        &self.layers
+    }
+
+    pub fn get_layers_mut(&mut self) -> &mut Vec<Layer> {
+        &mut self.layers
+    }
+}
+
+pub struct IdIterator<'a> {
+    curr_group_idx: usize,
+    curr_group_object_idx: usize,
+    groups: &'a [Layer],
+}
+
+impl<'a> IdIterator<'a> {
+    pub fn new(groups: &'a [Layer]) -> IdIterator<'a> {
+        Self {
+            groups,
+            curr_group_idx: 0,
+            curr_group_object_idx: 0,
+        }
+    }
+}
+
+impl<'a> Iterator for IdIterator<'a> {
+    type Item = &'a str;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // get a current group, or bubble up the `None`
+        let group = self.groups.get(self.curr_group_idx)?;
+
+        if let Some(group_object) = group.content.get(self.curr_group_object_idx) {
+            // match on the group object to see if this element is a rectangle or image,
+            // and therefore contains `Identifier` information we can return from the iterator
+            match group_object {
+                object::Object::Rectangle(rect) => {
+                    self.curr_group_object_idx += 1;
+                    Some(&rect.ident.id)
+                }
+                object::Object::Image(image) => {
+                    self.curr_group_object_idx += 1;
+
+                    Some(&image.ident.id)
+                }
+                object::Object::Other(_) => {
+                    // we HAVE a valid object, but since its not an object we normally care
+                    // about, we have not parsed the identifiers for it
+                    self.curr_group_object_idx += 1;
+                    self.next()
+                }
+            }
+        } else {
+            // there are no more objects in this layer, go to the next layer
+            // group and return anything from there
+            self.curr_group_idx += 1;
+            self.curr_group_object_idx = 0;
+
+            self.next()
+        }
+    }
+}
+
+#[test]
+fn id_iterator() {
+    use quick_xml::events::BytesStart;
+
+    use object::{Identifiers, Image, Object, Rectangle};
+    let groups = vec![
+        Layer::eof_group_test(vec![]),
+        Layer::eof_group_test(vec![
+            Object::Rectangle(Rectangle::from_ident(Identifiers::zeros_with_id("1"))),
+            Object::Rectangle(Rectangle::from_ident(Identifiers::zeros_with_id("2"))),
+            Object::Image(Image::from_ident(Identifiers::zeros_with_id("3"))),
+        ]),
+        Layer::eof_group_test(vec![]),
+        Layer::eof_group_test(vec![
+            Object::Rectangle(Rectangle::from_ident(Identifiers::zeros_with_id("4"))),
+            Object::Other(Event::Empty(BytesStart::owned_name(
+                b"doesnt_matter".to_vec(),
+            ))),
+            Object::Other(Event::Empty(BytesStart::owned_name(
+                b"doesnt_matter2".to_vec(),
+            ))),
+            Object::Rectangle(Rectangle::from_ident(Identifiers::zeros_with_id("5"))),
+        ]),
+        Layer::eof_group_test(vec![]),
+    ];
+
+    let iter = IdIterator::new(&groups);
+    let ids = iter.collect::<Vec<_>>();
+    assert_eq!(&["1", "2", "3", "4", "5"], ids.as_slice());
+}
